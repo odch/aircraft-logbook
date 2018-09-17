39d21be3570b4f37c5063c498e79942da844d5af
Roland Züger <rzueger@tocco.ch>
2018-09-17 19:03:40 +0200

    Add Material UI

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Typography from '@material-ui/core/Typography'
 
-const Title = props => <div>{props.text}</div>
+const Title = props => (
+  <Typography variant="title" align="center">
+    {props.text}
+  </Typography>
+)
 
 Title.propTypes = {
   text: PropTypes.string.isRequired
