65d0343a119bef9fa769c60fcad06bcadeeb7445
Roland Züger <rzueger@tocco.ch>
2018-10-14 12:28:24 +0200

    Add LoadingIcon component

---
+++ src/components/LoadingIcon/LoadingIcon.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import withStyles from '@material-ui/core/styles/withStyles'
+
+const styles = () => ({
+  container: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
+  },
+  icon: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    margin: '-25px 0 0 -25px'
+  }
+})
+
+const LoadingIcon = props => (
+  <div className={props.classes.container}>
+    <CircularProgress size={props.size} className={props.classes.icon} />
+  </div>
+)
+
+LoadingIcon.propTypes = {
+  classes: PropTypes.object.isRequired,
+  size: PropTypes.number
+}
+
+export default withStyles(styles)(LoadingIcon)
