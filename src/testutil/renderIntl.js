159c3fd8286be65e2c4aad402da628c83204ec04
Roland Züger <rzueger@tocco.ch>
2018-10-19 06:36:23 +0200

    Add possibility to write tests for some Material components
    Some of the components (i.e. <Dialog>) cannot be rendered using
    the react-test-renderer module
    -> Use `createMount` from the material test utils for those cases
       (requires `enzyme`
    -> Maybe we can switch from `react-test-renderer` to `enzyme` at some
       point

--- src/testutil/renderIntl.js
+++ src/testutil/renderIntl.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { createMount } from '@material-ui/core/test-utils'
 import { IntlProvider } from 'react-intl'
 import messages from '../messages'
 
 const LOCALE = 'de'
 
-const renderIntl = component =>
-  renderer.create(
-    <IntlProvider locale={LOCALE} messages={messages[LOCALE]}>
-      {component}
-    </IntlProvider>
-  )
+const renderInIntlProvider = component => (
+  <IntlProvider locale={LOCALE} messages={messages[LOCALE]}>
+    {component}
+  </IntlProvider>
+)
+
+const renderIntl = component => renderer.create(renderInIntlProvider(component))
+
+export const renderIntlMaterial = component =>
+  createMount()(renderInIntlProvider(component))
 
 export default renderIntl
