223c21a346869c34fb7f585b86e98b7140e230e7
Roland Züger <rzueger@tocco.ch>
2018-10-27 20:30:12 +0200

    Use direct import for `withStyles`

--- src/components/DeleteButton/DeleteButton.js
+++ src/components/DeleteButton/DeleteButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Button from '@material-ui/core/Button'
-import { withStyles } from '@material-ui/core/styles'
+import withStyles from '@material-ui/core/styles/withStyles'
 import DeleteIcon from '@material-ui/icons/Delete'
 
 const styles = theme => ({
