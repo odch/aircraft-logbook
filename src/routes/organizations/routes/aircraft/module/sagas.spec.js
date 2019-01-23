511b470694652b52cb1100e0cc7ac297ed157fa7
Roland Züger <rzueger@tocco.ch>
2019-01-23 06:17:24 +0100

    Fix null pointer in #getCurrentMember saga

--- src/routes/organizations/routes/aircraft/module/sagas.spec.js
+++ src/routes/organizations/routes/aircraft/module/sagas.spec.js
@@ -698,6 +698,38 @@ describe('routes', () => {
             })
           })
 
+          describe('getCurrentMember', () => {
+            it('should return the current member', () => {
+              const member1 = {
+                firstname: 'Max',
+                lastname: 'Muster',
+                user: undefined
+              }
+              const member2 = {
+                firstname: 'Hans',
+                lastname: 'Meier',
+                user: {
+                  id: 'o7flC7jw8j'
+                }
+              }
+              return expectSaga(sagas.getCurrentMember)
+                .withState({
+                  firebase: {
+                    auth: {
+                      uid: 'o7flC7jw8j'
+                    }
+                  },
+                  firestore: {
+                    ordered: {
+                      organizationMembers: [member1, member2]
+                    }
+                  }
+                })
+                .returns(member2)
+                .run()
+            })
+          })
+
           describe('default', () => {
             it('should fork all sagas', () => {
               const generator = sagas.default()
