4a333a7693aefa36c21be9d842883b814ebcf826
Roland Züger <rzueger@tocco.ch>
2019-07-28 11:38:19 +0200

    Upgrade Firebase to 6.3.3 and replace "@firebase/..." dependencies
    "@firebase/..." dependencies shouldn't be used directly (only add
    "firebase" dependency and import the modules you need).

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -4,14 +4,12 @@ export const initFirebase = store => {
   initPromise = Promise.all([
     import('react-redux-firebase'),
     import('redux-firestore'),
-    import('@firebase/app'),
-    import('@firebase/auth'),
-    import('@firebase/firestore'),
-    window.Cypress && import('@firebase/functions')
+    import('firebase/app'),
+    import('firebase/auth'),
+    import('firebase/firestore'),
+    window.Cypress && import('firebase/functions')
   ]).then(([reactReduxFirebase, reduxFirestore, firebase]) => {
     firebase.default.initializeApp(__CONF__.firebase)
-
-    firebase.default.firestore().settings({ timestampsInSnapshots: true })
 
     const reactReduxFirebaseConfig = {
       userProfile: 'users',
