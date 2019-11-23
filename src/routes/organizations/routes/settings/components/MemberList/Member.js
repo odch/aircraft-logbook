192526eb2ee1e8e1ea94ee9c91bfb74395ca9f07
Roland Züger <rzueger@tocco.ch>
2019-11-23 16:24:06 +0100

    Organization settings: mark members who haven't joined yet
    - `user` ref is missing on members who haven't joined
    - those are put at the end of the members list and displayed with
      lighter and italic font and with a tooltip

---
+++ src/routes/organizations/routes/settings/components/MemberList/Member.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { injectIntl } from 'react-intl'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import IconButton from '@material-ui/core/IconButton'
+import DeleteIcon from '@material-ui/icons/Delete'
+import {
+  member as memberShape,
+  intl as intlShape
+} from '../../../../../../shapes'
+import Tooltip from '@material-ui/core/Tooltip'
+import { withStyles } from '@material-ui/core'
+
+const styles = {
+  pending: {
+    fontStyle: 'italic',
+    color: '#AAAAAA'
+  }
+}
+
+class Member extends React.Component {
+  getRoleNames = roles =>
+    roles
+      ? roles.map(role => this.msg(`organization.role.${role}`)).join(', ')
+      : ''
+
+  msg = id => this.props.intl.formatMessage({ id })
+
+  render() {
+    const { member, openDeleteMemberDialog, classes } = this.props
+
+    const invitePending = !member.user
+
+    const itemText = (
+      <ListItemText
+        primary={`${member.lastname} ${member.firstname}${
+          member.nr ? ` (${member.nr})` : ''
+        }`}
+        secondary={this.getRoleNames(member.roles)}
+      />
+    )
+
+    return (
+      <ListItem
+        key={member.id}
+        disableGutters
+        className={invitePending ? classes.pending : undefined}
+        divider
+      >
+        {invitePending ? (
+          <Tooltip
+            title={this.msg('organization.invite.pending')}
+            placement="bottom-start"
+          >
+            {itemText}
+          </Tooltip>
+        ) : (
+          itemText
+        )}
+        <ListItemSecondaryAction>
+          <IconButton onClick={() => openDeleteMemberDialog(member)}>
+            <DeleteIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    )
+  }
+}
+
+Member.propTypes = {
+  member: memberShape.isRequired,
+  openDeleteMemberDialog: PropTypes.func.isRequired,
+  intl: intlShape.isRequired,
+  classes: PropTypes.object.isRequired
+}
+
+export default injectIntl(withStyles(styles)(Member))
