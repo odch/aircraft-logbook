21da7b5acb81026aa6b91c499547b9163951dfb6
Roland Züger <rzueger@tocco.ch>
2019-12-08 15:28:16 +0100

    Show short airport identifier only (e.g. LSZT instead of Lommis)

--- src/routes/organizations/routes/aircraft/components/FlightList/FlightList.spec.js
+++ src/routes/organizations/routes/aircraft/components/FlightList/FlightList.spec.js
@@ -12,8 +12,8 @@ describe('routes', () => {
               const flights = [
                 {
                   id: 'sStfyLd2XArT7oUZPFDn',
-                  departureAerodrome: { name: 'Lommis' },
-                  destinationAerodrome: { name: 'Lommis' },
+                  departureAerodrome: { identification: 'LSZT' },
+                  destinationAerodrome: { identification: 'LSZT' },
                   blockOffTime: {
                     toDate: () => Date.parse('2018-11-20 10:00')
                   },
@@ -33,8 +33,8 @@ describe('routes', () => {
                 },
                 {
                   id: 'sStfyLd2XArT7oUZPFDa',
-                  departureAerodrome: { name: 'Lommis' },
-                  destinationAerodrome: { name: 'Lommis' },
+                  departureAerodrome: { identification: 'LSZT' },
+                  destinationAerodrome: { identification: 'LSZT' },
                   blockOffTime: {
                     toDate: () => Date.parse('2018-11-21 10:00')
                   },
