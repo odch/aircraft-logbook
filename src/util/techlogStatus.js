1ee590095753832323b84feaccd98788538cb6e6
Roland Züger <rzueger@tocco.ch>
2020-08-22 15:37:53 -0500

    Enable "for information only" techlog entry status only for managers
    Note: In the flight create dialog this status isn't shown either even
          if the user is a techlog manager. "For information only" is
          a special status that can be used, for instance, to announce
          a 100h inspection that will take place soon. So, this status
          isn't needed in the flight create dialog.

--- src/util/techlogStatus.js
+++ src/util/techlogStatus.js
@@ -1,20 +1,22 @@
 const STATUS = [
-  { id: 'for_information_only', closed: false },
-  { id: 'not_airworthy', closed: false },
-  { id: 'not_flight_relevant', closed: false },
-  { id: 'closed', closed: true },
-  { id: 'crs', closed: true },
-  { id: 'crs_check', closed: true },
-  { id: 'annual_review', closed: true }
+  { id: 'for_information_only', closed: false, requiresManager: true },
+  { id: 'not_airworthy', closed: false, requiresManager: false },
+  { id: 'not_flight_relevant', closed: false, requiresManager: false },
+  { id: 'closed', closed: true, requiresManager: true },
+  { id: 'crs', closed: true, requiresManager: true },
+  { id: 'crs_check', closed: true, requiresManager: true },
+  { id: 'annual_review', closed: true, requiresManager: true }
 ]
 
 export const getTechlogStatus = isTechlogManager =>
   isTechlogManager === true
     ? STATUS.slice(0)
-    : STATUS.filter(status => status.closed === false)
+    : STATUS.filter(status => status.requiresManager === false)
 
 export const getOpenTechlogStatus = () =>
-  STATUS.filter(status => status.closed === false)
+  STATUS.filter(
+    status => status.closed === false && status.requiresManager === false
+  )
 
 export const isClosed = statusId => {
   const status = STATUS.find(status => status.id === statusId)
