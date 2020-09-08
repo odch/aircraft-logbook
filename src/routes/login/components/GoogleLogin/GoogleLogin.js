90534a192d0117937779ff98851a589b28be3336
Roland Züger <rzueger@tocco.ch>
2020-09-08 07:50:38 -0500

    Bump file-loader to 6.1.0

--- src/routes/login/components/GoogleLogin/GoogleLogin.js
+++ src/routes/login/components/GoogleLogin/GoogleLogin.js
@@ -4,8 +4,7 @@ import { FormattedMessage } from 'react-intl'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import withStyles from '@material-ui/core/styles/withStyles'
-
-const logo = require('./Google_G_Logo.svg')
+import logo from './Google_G_Logo.svg'
 
 const styles = theme => ({
   failureMessage: {
