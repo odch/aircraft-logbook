2bc1a3b5892909e7edb8a501fb948daa8a42c46c
Roland Züger <rzueger@tocco.ch>
2020-09-12 08:56:19 -0500

    Introduce prop `fractionDigits` for DecimalField component
    The value of the prop can either be `1` or `2` -> `2` is the default

--- src/components/DecimalField/DecimalField.js
+++ src/components/DecimalField/DecimalField.js
@@ -2,13 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import TextField from '@material-ui/core/TextField'
 
-const formatWithTwoDecimals = value => Number(value).toFixed(2)
+const formatDecimalNumber = (value, fractionDigits = 2) =>
+  Number(value).toFixed(fractionDigits)
 
 class DecimalField extends React.Component {
   state = {
     stringValue:
       typeof this.props.value === 'number'
-        ? formatWithTwoDecimals(this.props.value / 100)
+        ? formatDecimalNumber(this.props.value / 100, this.props.fractionDigits)
         : ''
   }
 
@@ -30,21 +31,23 @@ class DecimalField extends React.Component {
   }
 
   handleBlur = () => {
-    const { onChange } = this.props
+    const { onChange, fractionDigits } = this.props
 
     this.numberInput.current.removeEventListener('wheel', this.handleWheel)
 
     const floatValue = parseFloat(this.state.stringValue)
     if (!isNaN(floatValue)) {
-      const twoDecimals = Math.round(floatValue * 100) / 100
-      const stringValue = formatWithTwoDecimals(twoDecimals)
+      const roundingFactor = fractionDigits === 1 ? 10 : 100
+      const roundedNumber =
+        Math.round(floatValue * roundingFactor) / roundingFactor
+      const stringValue = formatDecimalNumber(roundedNumber, fractionDigits)
 
       this.setState({
         stringValue
       })
 
       if (onChange) {
-        const hundredths = Math.round(twoDecimals * 100)
+        const hundredths = Math.round(roundedNumber * 100)
         onChange(hundredths)
       }
     } else if (this.state.stringValue === '' && onChange) {
@@ -57,7 +60,15 @@ class DecimalField extends React.Component {
   }
 
   render() {
-    const { label, cy, margin, fullWidth, error, disabled } = this.props
+    const {
+      label,
+      cy,
+      margin,
+      fullWidth,
+      error,
+      disabled,
+      fractionDigits
+    } = this.props
     const { stringValue } = this.state
     return (
       <TextField
@@ -68,7 +79,7 @@ class DecimalField extends React.Component {
         onBlur={this.handleBlur}
         inputRef={this.numberInput}
         type="number"
-        inputProps={{ step: '.01' }}
+        inputProps={{ step: fractionDigits === 1 ? '0.1' : '.01' }}
         data-cy={cy}
         margin={margin}
         fullWidth={fullWidth}
@@ -87,7 +98,8 @@ DecimalField.propTypes = {
   fullWidth: PropTypes.bool,
   onChange: PropTypes.func,
   error: PropTypes.bool,
-  disabled: PropTypes.bool
+  disabled: PropTypes.bool,
+  fractionDigits: PropTypes.oneOf([1, 2])
 }
 
 export default DecimalField
