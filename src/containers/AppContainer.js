8103bc824759c11974cb33c1fa8e53cd3bc6a3c1
Roland Züger <rzueger@tocco.ch>
2020-09-17 12:18:42 -0500

    Set 'en' as default language
    Language can be controlled by user by setting `locale` to the desired
    locale short code on the user document in the firestore database
    (not possible to change the language in the UI currently)

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -4,7 +4,7 @@ import App from '../components/App'
 import { watchAerodromes } from '../modules/app'
 
 const DEFAULT_AUTH = { isLoaded: false, isEmpty: true }
-const DEFAULT_LOCALE = 'de'
+const DEFAULT_LOCALE = 'en'
 
 const mapStateToProps = (state /*, ownProps*/) => ({
   auth: state.firebase ? state.firebase.auth : DEFAULT_AUTH,
