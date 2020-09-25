364b3aa3d98bf95f2ddaa11b9835834b34f45d3e
Roland Züger <rzueger@tocco.ch>
2020-09-25 12:33:49 -0500

    Add aircraft setting techlogSignatureEnabled to hide/show eSignature

--- src/routes/organizations/routes/aircraft/util/flightDialogUtils.js
+++ src/routes/organizations/routes/aircraft/util/flightDialogUtils.js
@@ -32,7 +32,8 @@ export const aircraftSettings = (state, aircraftId) => {
         aircraftSettings.engineHoursCounterEnabled === true,
       engineHoursCounterFractionDigits:
         aircraftSettings.engineHoursCounterFractionDigits,
-      techlogEnabled: aircraftSettings.techlogEnabled === true
+      techlogEnabled: aircraftSettings.techlogEnabled === true,
+      techlogSignatureEnabled: aircraftSettings.techlogSignatureEnabled === true
     }
   }
 }
