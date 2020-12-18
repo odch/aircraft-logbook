e6310afde3cf5af354646a0f14c4577062b32a99
Philipp Hug <philipp@hug.cx>
2020-12-18 11:56:06 +0100

    Update nature of flights for mfgt

--- src/util/flightNatures.js
+++ src/util/flightNatures.js
@@ -2,12 +2,9 @@ const NATURES = [
   'vp', // charter vfr
   'ip', // charter ifr
   'vs', // instruction vfr
-  'vss', // instruction vfr solo
   'is', // instruction ifr
-  'iss', // instruction ifr solo
-  'tr', // trial flight
   'tec', // technical flight
-  'vc' // "commercial" sight seeing flight
+  'xr', // Rundflug
 ]
 
 export const getFlightNatures = () => NATURES.slice(0)
